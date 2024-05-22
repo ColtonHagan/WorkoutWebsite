330bd05b490b2063bcfa6625b4dbaa6a875934db
ColtonHagan <coltonmhagan@gmail.com>
2024-05-22 15:59:24 -0700

    planning and setup is done :)

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './views/Home';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
