8f0403054bdf619bf023f40d33bd875e3dae383e
ColtonHagan <coltonmhagan@gmail.com>
2024-11-20 12:04:42 -0800

    updating to hash router to better work with gh-pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="WorkoutWebsite">
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
